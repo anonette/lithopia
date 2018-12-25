e6304695bea1811e9c75812ab77369a943cdc878
yair <yair99@gmail.com>
2018-12-25 22:26:46 +0200

    Create logic.js

---
+++ logic.js
@@ -0,0 +1,95 @@
+/* global getParticipantRegistry getAssetRegistry getFactory */
+/**
+* A satellite color reading has been received 
+* @param {org.lithopia.basic.Flagcolor} flagcolor - the Flagcolor transaction
+* @transaction
+*/
+async function flagColor(flagcolor) {  // eslint-disable-line no-unused-vars
+  const place = flagcolor.place;
+  //console.log('Checking the flag color ' + flagcolor.flagColor + ' to place ' + flagcolor.$name);
+  if (place.flagColors) {
+  place.flagColors.unshift(flagcolor);
+  place.requestSources.unshift(flagcolor);
+  place.datasetIds.unshift(flagcolor); 
+  } else {
+  place.flagColors = [flagcolor];
+  place.requestSources = [flagcolor]; 
+  place.datasetIds = [flagcolor]; 
+  }
+    
+  // add the satelliteReading  to the Lithopian Place
+  const placeRegistry = await getAssetRegistry('org.lithopia.basic.LithopiaPlace');
+  await placeRegistry.update(place);
+}
+  /** 
+    * LithopiaPlaceSold transaction triggered by satellite data changing the owner of a property
+    * @param {org.lithopia.basic.LithopiaPlaceSold} lithopiaPlaceSold - the LithopianPlaceSold transaction
+    * @transaction
+    */
+  async function selling(lithopiaPlaceSold) {  // eslint-disable-line no-unused-vars
+  const place = lithopiaPlaceSold.place;
+  const flag = place.flagColors[0].flagColor;  
+  // if thecolor didn't change, the owner remains
+  if (flag === 'red'){
+  place.owner = lithopiaPlaceSold.newOwner.name;
+  }
+  else{
+  place.owner = lithopiaPlaceSold.place.owner;
+  }
+  // update the newOwner
+  const assetRegistry = await getAssetRegistry('org.lithopia.basic.LithopiaPlace');
+  await assetRegistry.update(place);
+// emit who is the old or new owner
+let placeEvent = getFactory().newEvent('org.lithopia.basic', 'LithopiaPlaceTransactions');
+placeEvent.place = lithopiaPlaceSold.place;
+placeEvent.newOwner = place.owner;
+emit(placeEvent);
+}
+/**
+  * Lithopians entering partnership contract 
+  * @param {org.lithopia.basic.Partners} partnering - the Partners transaction
+  * @transaction
+  */
+  async function partnering(partnering) {  // eslint-disable-line no-unused-vars
+  const contract = partnering.contract;
+  if (contract.partners) {
+  contract.partners.push(partnering);
+  }
+  else{
+  contract.partners = [partnering];
+  }
+  // add the partners to the contract
+const placeRegistry = await getAssetRegistry('org.lithopia.basic.LithopiaMarriage');
+await placeRegistry.update(contract);
+// emit the new partnership in Lithopia
+let partnerEvent = getFactory().newEvent('org.lithopia.basic', 'NewPartnershipinLithopia');
+partnerEvent.contract = partnering.contract;
+partnerEvent.partners = partnering.contract.partners;
+emit(partnerEvent);
+}  
+/** 
+* LithopiaMarriage transaction changng the status of some partners
+* @param {org.lithopia.basic.Marriage} marrying - the LithopianPlaceSold transaction
+* @transaction
+*/
+async function married(marrying) {  // eslint-disable-line no-unused-vars
+const partner = marrying.partnerMarriage;
+const partners = marrying.contract.partners;
+//console.log(Object.values(partners));
+//console.log(partners[0].newPartner);
+//console.log(partner.name);
+//console.log(partner);
+let i;
+for (i=0; i < partners.length; i++){
+   if (partners[i].newPartner.includes(partner.name)){
+partner.status = "Married";
+}
+else
+{
+partner.status = "Single";
+}
+}
+// change the status of the partnership based on enum
+const participantRegistry = await getParticipantRegistry('org.lithopia.basic.Lithopian');
+await participantRegistry.update(partner);
+}
